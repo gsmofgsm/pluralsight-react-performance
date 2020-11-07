fb5d52d8d2799b6d106be82069db4d1321b69e05
Qing Ma <gsmofgsm@hotmail.com>
2020-11-07 14:57:58 +0100

    change Summary to a function component

--- src/Summary.js
+++ src/Summary.js
@@ -1,49 +1,36 @@
 import React from "react";
 import levenshtein from "levenshtein";
 
-export class Summary extends React.Component {
-  shouldComponentUpdate(nextProps) {
-    const oldKeys = Object.keys(this.props.cards);
-    const newKeys = Object.keys(nextProps.cards);
+export const Summary = (props) => {
+  const cards = Object.values(props.cards);
 
-    console.log({
-      oldLength: oldKeys.length,
-      newLength: newKeys.length,
+  const distances = { max: 0, min: 100000 };
+  cards.forEach((currentCard) => {
+    cards.forEach((compareCard) => {
+      if (compareCard === currentCard) {
+        return;
+      }
+      const distance = levenshtein(currentCard.label, compareCard.label);
+
+      distances.max = Math.max(distances.max, distance);
+      distances.min = Math.min(distances.min, distance);
     });
+  });
 
-    return oldKeys.length !== newKeys.length;
-  }
-  render() {
-    const cards = Object.values(this.props.cards);
-
-    const distances = { max: 0, min: 100000 };
-    cards.forEach((currentCard) => {
-      cards.forEach((compareCard) => {
-        if (compareCard === currentCard) {
-          return;
-        }
-        const distance = levenshtein(currentCard.label, compareCard.label);
-
-        distances.max = Math.max(distances.max, distance);
-        distances.min = Math.min(distances.min, distance);
-      });
-    });
-
-    return (
-      <div
-        style={{
-          position: "absolute",
-          right: 20,
-          top: 20,
-          backgroundColor: "#fafafa",
-          padding: "10px",
-          border: "3px solid #333",
-        }}
-      >
-        <div>You have {Object.keys(this.props.cards).length} cards!</div>
-        <div>Max difference in labels: {distances.max}</div>
-        <div>Min difference in labels: {distances.min}</div>
-      </div>
-    );
-  }
-}
+  return (
+    <div
+      style={{
+        position: "absolute",
+        right: 20,
+        top: 20,
+        backgroundColor: "#fafafa",
+        padding: "10px",
+        border: "3px solid #333",
+      }}
+    >
+      <div>You have {Object.keys(props.cards).length} cards!</div>
+      <div>Max difference in labels: {distances.max}</div>
+      <div>Min difference in labels: {distances.min}</div>
+    </div>
+  );
+};
