4a61701ec8a2bbfa80c57f07e9daea8514427a03
Qing Ma <gsmofgsm@hotmail.com>
2020-11-07 15:16:51 +0100

    make Card a function component

--- src/Card.js
+++ src/Card.js
@@ -1,33 +1,31 @@
 import React from "react";
 
-export class Card extends React.Component {
-  render() {
-    const { card, onDragStart, onDragEnd, onDoubleClick } = this.props;
+export const Card = (props) => {
+  const { card, onDragStart, onDragEnd, onDoubleClick } = props;
 
-    return (
-      <div
-        onMouseDown={ev => {
-          const clickOffset = {
-            x: ev.clientX - parseFloat(ev.currentTarget.style.left),
-            y: ev.clientY - parseFloat(ev.currentTarget.style.top)
-          };
-          onDragStart(clickOffset);
-        }}
-        onMouseUp={onDragEnd}
-        onDoubleClick={onDoubleClick}
-        style={{
-          position: "absolute",
-          left: card.position.left,
-          top: card.position.top,
-          backgroundColor: "#fff",
-          padding: "25px",
-          cursor: "move",
-          userSelect: "none"
-        }}
-        key={card.id}
-      >
-        {card.label}
-      </div>
-    );
-  }
-}
+  return (
+    <div
+      onMouseDown={(ev) => {
+        const clickOffset = {
+          x: ev.clientX - parseFloat(ev.currentTarget.style.left),
+          y: ev.clientY - parseFloat(ev.currentTarget.style.top),
+        };
+        onDragStart(clickOffset);
+      }}
+      onMouseUp={onDragEnd}
+      onDoubleClick={onDoubleClick}
+      style={{
+        position: "absolute",
+        left: card.position.left,
+        top: card.position.top,
+        backgroundColor: "#fff",
+        padding: "25px",
+        cursor: "move",
+        userSelect: "none",
+      }}
+      key={card.id}
+    >
+      {card.label}
+    </div>
+  );
+};
