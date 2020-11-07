69c573c0d3ab1a87896a86fabf64d64ca452f618
Qing Ma <gsmofgsm@hotmail.com>
2020-11-07 15:12:51 +0100

    React.memo and condition when props are the same

--- src/Summary.js
+++ src/Summary.js
@@ -1,36 +1,39 @@
 import React from "react";
 import levenshtein from "levenshtein";
 
-export const Summary = (props) => {
-  const cards = Object.values(props.cards);
+export const Summary = React.memo(
+  (props) => {
+    const cards = Object.values(props.cards);
 
-  const distances = { max: 0, min: 100000 };
-  cards.forEach((currentCard) => {
-    cards.forEach((compareCard) => {
-      if (compareCard === currentCard) {
-        return;
-      }
-      const distance = levenshtein(currentCard.label, compareCard.label);
+    const distances = { max: 0, min: 100000 };
+    cards.forEach((currentCard) => {
+      cards.forEach((compareCard) => {
+        if (compareCard === currentCard) {
+          return;
+        }
+        const distance = levenshtein(currentCard.label, compareCard.label);
 
-      distances.max = Math.max(distances.max, distance);
-      distances.min = Math.min(distances.min, distance);
+        distances.max = Math.max(distances.max, distance);
+        distances.min = Math.min(distances.min, distance);
+      });
     });
-  });
 
-  return (
-    <div
-      style={{
-        position: "absolute",
-        right: 20,
-        top: 20,
-        backgroundColor: "#fafafa",
-        padding: "10px",
-        border: "3px solid #333",
-      }}
-    >
-      <div>You have {Object.keys(props.cards).length} cards!</div>
-      <div>Max difference in labels: {distances.max}</div>
-      <div>Min difference in labels: {distances.min}</div>
-    </div>
-  );
-};
+    return (
+      <div
+        style={{
+          position: "absolute",
+          right: 20,
+          top: 20,
+          backgroundColor: "#fafafa",
+          padding: "10px",
+          border: "3px solid #333",
+        }}
+      >
+        <div>You have {Object.keys(props.cards).length} cards!</div>
+        <div>Max difference in labels: {distances.max}</div>
+        <div>Min difference in labels: {distances.min}</div>
+      </div>
+    );
+  },
+  (p1, p2) => Object.keys(p1.cards).length === Object.keys(p2.cards).length
+);
